189380caf00f1bfc69666729d0ca794afbc6a5ee
Duncan Freeman <duncan.freeman1@gmail.com>
2024-01-13 03:13:16 -0800

    Modifiable b-field

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use egui::{Align2, Color32, Stroke};
+use egui::{Align2, Color32, DragValue, Stroke, Ui};
+use quantum::{SpinState, SZ_POSITIVE_STATE, Complex, b_field};
 use threegui::{ThreeUi, Vec3};
+
+mod quantum;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -49,18 +52,17 @@ fn main() {
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    value: f32,
+    theta: f32,
+    initial_state: SpinState,
+    time: f32,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            theta: 0.,
+            initial_state: quantum::SZ_POSITIVE_STATE,
+            time: 0.,
         }
     }
 }
@@ -72,9 +74,33 @@ impl TemplateApp {
     }
 }
 
+fn edit_complex(ui: &mut Ui, cpx: &mut Complex, name: &str) {
+    ui.horizontal(|ui| {
+        ui.label(name);
+        ui.add(DragValue::new(&mut cpx.re).prefix("Re: "));
+        ui.add(DragValue::new(&mut cpx.im).prefix("Im: "));
+    });
+}
+
 impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::SidePanel::left("panel").show(ctx, |ui| {
+            ui.strong("Parameters");
+            ui.add(DragValue::new(&mut self.time).prefix("Time: ").speed(1e-2));
+            ui.add(
+                DragValue::new(&mut self.theta)
+                    .prefix("Angle θ: ")
+                    .suffix(" rads")
+                    .speed(1e-2),
+            );
+
+            ui.strong("Initial state");
+            edit_complex(ui, &mut self.initial_state.x, "A: ");
+            edit_complex(ui, &mut self.initial_state.y, "B: ");
+            // TODO: Normalize button
+        });
+
         egui::CentralPanel::default()
             .show(ctx, |ui| threegui::threegui(ui, |three| self.ui_3d(three)));
     }
@@ -83,24 +109,25 @@ impl eframe::App for TemplateApp {
 impl TemplateApp {
     fn ui_3d(&mut self, three: &mut ThreeUi) {
         axes(three);
+        let b_field: mint::Vector3<f32> = b_field(self.theta).into();
+        label_line(three, b_field.into(), Color32::YELLOW, "B");
     }
 }
 
 fn axes(three: &mut ThreeUi) {
-    let paint = three.painter();
+    label_line(three, Vec3::X, Color32::RED, "X");
+    label_line(three, Vec3::Y, Color32::GREEN, "Y");
+    label_line(three, Vec3::Z, Color32::LIGHT_BLUE, "Z");
+}
 
-    for (dim, color, name) in [
-        (Vec3::X, Color32::RED, "X"),
-        (Vec3::Y, Color32::GREEN, "Y"),
-        (Vec3::Z, Color32::LIGHT_BLUE, "Z"),
-    ] {
-        paint.line(Vec3::ZERO, dim, Stroke::new(1., color));
-        paint.text(
-            dim * 1.1,
-            Align2::CENTER_CENTER,
-            name,
-            Default::default(),
-            color,
-        );
-    }
+fn label_line(three: &mut ThreeUi, v: Vec3, color: Color32, name: &str) {
+    let paint = three.painter();
+    paint.line(Vec3::ZERO, v, Stroke::new(1., color));
+    paint.text(
+        v.normalize() * (v.length() + 0.1),
+        Align2::CENTER_CENTER,
+        name,
+        Default::default(),
+        color,
+    );
 }
